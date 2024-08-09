d81d1216b7cc556c60813bad533be132c5c591b1
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-08-09 14:31:13 +0500

    14:31

--- kahoot_app/views.py
+++ kahoot_app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+
+# Import Pagination
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
     categories = Category.objects.all()
+
+    p = Paginator(Category.objects.all(), 10)
+    page = request.GET.get('page')
+    page_number = p.get_page(page)
+
+    q = request.GET.get('q')
+    if q:
+        categories = Category.objects.filter(title__icontais=q)
     context = {
         'categories': categories,
+        'page_number': page_number,
     }
     return render(request, 'kahoot/home.html', context)
 
