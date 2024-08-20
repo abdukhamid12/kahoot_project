5e2a00c4fa9c14b1346440c2936fb94d4e9a826c
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-08-20 17:06:49 +0500

    17:06

--- kahoot_app/models.py
+++ kahoot_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     author = models.CharField(max_length=100)
     logo = models.ImageField(upload_to='cat_logo/')
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
--- kahoot_app/views.py
+++ kahoot_app/views.py
@@ -20,7 +20,7 @@ def home(request):
 
     context = {
         'categories': page_number,
-        'search': search,
+        'search': search if search else '',
         'page_number': page_number,
     }
     return render(request, 'kahoot/home.html', context)
@@ -33,22 +33,55 @@ def detail_view(request, pk):
     return render(request, 'kahoot/detail.html', context)
 
 def list_create(request):
+    category_form = CategoryForm()
+    question_form = QuestionForm()
+    option_form = OptionForm()
+    option_formset = OptionFormSet()
+
+    context = {
+        'category_form': category_form,
+        'question_form': question_form,
+        'option_form': option_form,
+        'option_formset': option_formset
+    }
+    return render(request, 'kahoot/category_style.html', context)
+
+
+def category_create(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('home-view')
+        else:
+            print(form.errors)
     category_form = CategoryForm()
-
-    if request.method == 'POST':
-        form = QuestionForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home-view')
     question_form = QuestionForm()
+    option_form = OptionForm()
+    option_formset = OptionFormSet()
 
     context = {
         'category_form': category_form,
         'question_form': question_form,
+        'option_form': option_form,
+        'option_formset': option_formset
     }
-    return render(request, 'kahoot/category_style.html', context)
+    return render(request, 'kahoot/create_page.html', context)
+
+
+def create_question(request):
+    if request.method == 'POST':
+        question_form = QuestionForm(request.POST, request.FILES)
+        option_formset = OptionFormSet(request.POST, instance=question_form.instance)
+        if question_form.is_valid() and option_formset.is_valid():
+            question = question_form.save()
+            option_formset.save()
+            return redirect('home-view')  # Replace with your success URL
+    else:
+        question_form = QuestionForm()
+        option_formset = OptionFormSet()
+
+    return render(request, 'kahoot/create_page.html', {
+        'question_form': question_form,
+        'option_formset': option_formset,
+    })
